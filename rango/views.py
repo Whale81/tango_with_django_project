d67c1ea8ecf17a3d727f2a51a8a5954d9add29bc
Zhen Ruan <2568785R@student.gla.ac.uk>
2021-07-30 12:04:10 +0800

    chapter9.8 Restricting Access with a Decorator

--- rango/views.py
+++ rango/views.py
@@ -8,9 +8,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
-from django.urls import reverse
-from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -204,3 +202,7 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")   
